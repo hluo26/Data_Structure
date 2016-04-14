afab05a3fc3cd71e5654da0755fcf233da88feff
hluo26 <h965l741@ku.edu>
2016-04-13 19:02:49 -0500

    Update and rename main.cc to main.cpp

---
+++ pokemon/main.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+#include<string>
+#include"Colosseum.h"
+#include"Pokemon.h"
+#include"Dice.h"
+
+int main()
+{
+	Pokemon poke1;
+	Pokemon poke2;
+
+	Colosseum c1;
+
+	bool ending =false;
+
+	std::string option= "";
+
+	while(!ending)
+	{
+		std::cout<<"Player 1, build your Pokemon!"<<std::endl;
+
+		c1.userBuild(poke1);
+
+		std::cout<<"Player 2, build your Pokemon!"<<std::endl;
+
+		c1.userBuild(poke2);
+
+		c1.play(poke1,poke2);
+
+		std::cout<<"Do you want to play again? (y/n): "<<std::endl;
+
+		std::cin>>option;
+
+		while(option !="n"&&option!="y")
+		{
+			std::cout<<"Wrong options!"<<std::endl;
+			std::cin>>option;
+			
+		}
+		if(option=="n")
+		{
+			std::cout<<"Thanks for playing!"<<std::endl;
+			ending =true;
+		}
+	}
+		
+		
+}
